c94fef9a8c6d5639c7dfeadcba17829eaa9ef332
Elvis <s49188705@gmail.com>
2022-11-28 16:53:25 +0300

    Using-Active-Model-Serializer

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,10 +6,20 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
-  def show
-    movie = Movie.find(params[:id])
-    render json: movie
-  end
+ def show
+   movie = Movie.find(params[:id])
+   render json: movie
+ end
+
+ def summary
+  movie = Movie.find(params[:id])
+  render json: movie, serializer: MovieSummarySerializer
+ end
+
+ def summaries
+  movies = Movie.all
+  render json: movies, each_serializer: MovieSummarySerializer
+ end
 
   private
 
